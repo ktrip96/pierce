4bf05886b0d16cec0f493af34fee60a09bb611e5
ktrip96 <ktrip96@gmail.com>
2022-08-11 13:24:50 +0300

    commit

--- pierce/pages/_app.js
+++ pierce/pages/_app.js
@@ -1,7 +1,12 @@
+// pages/_app.js
 import '../styles/globals.css'
-
+import { ChakraProvider } from '@chakra-ui/react'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
--- pierce/pages/index.js
+++ pierce/pages/index.js
@@ -1,9 +1,30 @@
 import styles from '../styles/Home.module.css'
+import { FcGoogle } from 'react-icons/fc'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.welcome_text}>Hello</h1>
+    <div className={styles.outter_container}>
+      <div className={styles.container}>
+        <h1 className={styles.welcome_text}>Welcome</h1>
+        <h1 className={styles.description}>Your opinion matters</h1>
+
+        <h3 className={styles.mini_description}>Evaluate your teachers </h3>
+        <img
+          src={'/students.png'}
+          alt='Students'
+          style={{
+            width: '300px',
+            height: '300px',
+            objectFit: 'cover',
+          }}
+        />
+        <div className={styles.google_button}>
+          <FcGoogle
+            style={{ width: '30px', height: '30px', marginRight: '10px' }}
+          />
+          Sign in with google
+        </div>
+      </div>
     </div>
   )
 }
