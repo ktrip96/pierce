61a4b7c079213435be4a82fe68ae14ff65906426
ktrip96 <ktrip96@gmail.com>
2022-08-12 12:26:43 +0300

    Animations

--- pierce/pages/index.js
+++ pierce/pages/index.js
@@ -1,23 +1,46 @@
 import styles from '../styles/Home.module.css'
 import { FcGoogle } from 'react-icons/fc'
+import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
     <div className={styles.outter_container}>
       <div className={styles.container}>
-        <h1 className={styles.welcome_text}>Welcome</h1>
-        <h1 className={styles.description}>Your opinion matters</h1>
-
-        <h3 className={styles.mini_description}>Evaluate your teachers </h3>
-        <img
-          src={'/students.png'}
-          alt='Students'
-          style={{
-            width: '300px',
-            height: '300px',
-            objectFit: 'cover',
+        <motion.h1
+          initial={{ scale: 0.5, opacity: 0 }}
+          animate={{
+            scale: 1,
+            opacity: 1,
+            transition: { delay: 0.4, duration: 0.6 },
           }}
-        />
+          className={styles.welcome_text}
+        >
+          Welcome
+        </motion.h1>
+        <motion.h1
+          initial={{ scale: 0.5, opacity: 0 }}
+          animate={{
+            scale: 1,
+            opacity: 1,
+            transition: { delay: 1.5, duration: 0.6 },
+          }}
+          className={styles.description}
+        >
+          Your opinion matters
+        </motion.h1>
+
+        <motion.h3
+          initial={{ scale: 0.5, opacity: 0 }}
+          animate={{
+            scale: 1,
+            opacity: 1,
+            transition: { delay: 2.5, duration: 0.6 },
+          }}
+          className={styles.mini_description}
+        >
+          Evaluate your teachers{' '}
+        </motion.h3>
+        <img src={'/students.png'} alt='Students' className={styles.image} />
         <div className={styles.google_button}>
           <FcGoogle
             style={{ width: '30px', height: '30px', marginRight: '10px' }}
